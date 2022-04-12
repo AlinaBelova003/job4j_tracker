e29eb36b3f882357f52854a1bb04761b0b28aaf0
alina <alinka_belova_2003@mail.ru>
2022-04-12 17:47:04 +0300

    5. Tracker - хранилище [#396 #287876]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,36 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        Item[] itemNew = new Item[size];
+        int size = 0;
+        for (int index = 0; index < size; index++) {
+            Item item = items[index];
+            if (item != null) {
+                itemNew[size] = item;
+                size++;
+        }
+
+    }
+        itemNew = Arrays.copyOf(itemNew, size);
+        for (int index = 0; index < itemNew.length; index++) {
+            System.out.println(itemNew[index]);
+        }
+            return itemNew;
+    }
+
+    public Item findByName(String kay) {
+        Item result = null;
+        for (int index = 0; index < items.length; index++) {
+            Item item = items[index];
+            if (item.getName().equals(kay)) {
+                result = item;
+                break;
+            }
+        }
+        return result;
     }
 
     public Item findById(int id) {
@@ -21,6 +53,5 @@ public class Tracker {
             }
         }
         return rsl;
-
     }
 }
