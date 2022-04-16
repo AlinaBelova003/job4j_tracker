502272649351de17a0ab53d00f1d1fb6dcac8a33
alina <alinka_belova_2003@mail.ru>
2022-04-16 15:55:14 +0300

    2.5. Реализация класса StartUI. Изменение заявки [#500746]

--- src/main/java/ru/job4j/tracker/StartUl.java
+++ src/main/java/ru/job4j/tracker/StartUl.java
@@ -15,7 +15,7 @@ public class StartUl {
                 String name = scanner.nextLine();
                 Item item = new Item(name);
                 tracker.add(item);
-            }  else if (select == 1) {
+            } else if (select == 1) {
                 System.out.println("=== Show all items ===");
                 Item[] items = tracker.findAll();
                 if (items.length > 0) {
@@ -24,6 +24,18 @@ public class StartUl {
                     }
                 } else {
                     System.out.println("Хранилище еще не содержит заявок");
+                }
+            } else if (select == 2) {
+                System.out.println("=== Edit item ===");
+                System.out.println("Enter id: ");
+                int id = Integer.parseInt(scanner.nextLine());
+                System.out.println("Enter name: ");
+                String name = scanner.nextLine();
+                Item item = new Item(name);
+                if (tracker.replace(id, item)) {
+                    System.out.println("Заявка изменена успешно.");
+                } else {
+                    System.out.println("Ошибка замены заявки.");
                 }
             } else if (select == 6) {
                 run = false;
