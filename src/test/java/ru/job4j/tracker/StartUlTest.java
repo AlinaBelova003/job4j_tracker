eadf6df4a58150aec5e9acf488fc9e12997c996d
alina <alinka_belova_2003@mail.ru>
2022-04-23 14:43:56 +0300

    9. Написать тесты на StartUI. [#785 #291772]

--- src/test/java/ru/job4j/tracker/StartUlTest.java
+++ src/test/java/ru/job4j/tracker/StartUlTest.java
@@ -11,44 +11,47 @@ public class StartUlTest {
 
     @Test
     public void whenCreateItem() {
+        Output out = new StubOutput();
         Input in = new StubInput(
                 new String[] {"0", "Item name", "1"}
                 );
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction(),
+                new CreateAction(out),
                 new Exit()
         };
-        new StartUl().init(in, tracker, actions);
+        new StartUl(out).init(in, tracker, actions);
         assertThat(tracker.findAll()[0].getName(), is("Item name"));
     }
 
     @Test
     public void whenReplaceItem() {
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Replaced item"));
         String replacedName = "New item name";
-        Input in = new StubInput(new String[] {"0", "1", "New item name", "1"  });
+        Input in = new StubInput(new String[] {"0", String.valueOf(item.getId()), "New item name", "1"  });
         UserAction[] actions = {
-                new EditAction(),
+                new EditAction(out),
                 new Exit()
         };
-        new StartUl().init(in, tracker, actions);
+        new StartUl(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()).getName(), is(replacedName));
     }
 
     @Test
     public void whenDeleteItem() {
+        Output out = new StubOutput();
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Deleted item"));
         Input in = new StubInput(
-                new String[] {"0", "1", "1"}
+                new String[] {"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
-                new DeleteAction(),
+                new DeleteAction(out),
                 new Exit()
         };
-        new StartUl().init(in, tracker, actions);
+        new StartUl(out).init(in, tracker, actions);
         assertThat(tracker.findById(item.getId()), is(nullValue()));
     }
 
