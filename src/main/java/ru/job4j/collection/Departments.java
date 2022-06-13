4de36912db216e47db54aeb37d7a94df249127e6
alina <alinka_belova_2003@mail.ru>
2022-06-13 11:31:31 +0300

    2. Отсортировать департаменты [#34352]

---
+++ src/main/java/ru/job4j/collection/Departments.java
@@ -0,0 +1,34 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+public class Departments {
+    public static List<String> fillGaps(List<String> deps) {
+        Set<String> tmp = new LinkedHashSet<>();
+        for (String value : deps) {
+            String start = "";
+            for (String el : value.split("/")) {
+                start = start.equals("") ? el : start + "/" + el;
+                tmp.add(start);
+            }
+        }
+        return new ArrayList<>(tmp);
+    }
+
+    public static void sortAsc(List<String> orgs) {
+        Collections.sort(orgs);
+    }
+
+    public static void sortDesc(List<String> orgs) {
+        Collections.sort(orgs, new DepDescComp());
+    }
+
+    public static void main(String[] args) {
+        List<String> input = Arrays.asList("k1/sk1/ssk1", "k2/sk1");
+        List<String> expect = Arrays.asList("k1", "k1/sk1", "k2", "k2/sk1");
+        List<String> result = Departments.fillGaps(input);
+        for (String rsl: result) {
+            System.out.println(rsl);
+        }
+    }
+}
