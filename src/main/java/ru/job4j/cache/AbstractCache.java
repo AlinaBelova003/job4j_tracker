f30eee5186f26d6e9b3550b8855b8ede0613dd76
alina <alinka_belova_2003@mail.ru>
2023-07-26 14:28:33 +0300

    1. Реализация кеша на SoftReference. Упрощение кода

--- src/main/java/ru/job4j/cache/AbstractCache.java
+++ src/main/java/ru/job4j/cache/AbstractCache.java
@@ -8,8 +8,7 @@ public abstract class AbstractCache<K, V> {
     private final Map<K, SoftReference<V>> cache = new HashMap<>();
 
     public final void put(K key, V value) {
-        SoftReference<V> softReference = new SoftReference<>(value);
-        cache.put(key, softReference);
+        cache.put(key, new SoftReference<>(value));
     }
 
     /**
@@ -18,19 +17,17 @@ public abstract class AbstractCache<K, V> {
      * Если ссылка есть в памяти, то по мягкой ссылки получаем значение и обновляем данные в кэши
      * Если нет, то загружаем и проверяем, если значение не пустое, то добавляем в кэш
      * @param key имя файла
+     * @return внутренние содержимое файла
      */
     public final V get(K key) {
-        if (cache.containsKey(key)) {
-            SoftReference<V> softReference = cache.get(key);
-            V value = softReference.get();
-            put(key, value);
+        SoftReference<V> softReference = cache.get(key);
+        V value = (softReference != null) ? softReference.get() : null;
+        if (value == null) {
+            value = load(key);
         } else {
-            V file = load(key);
-            if (file != null) {
-                put(key, file);
-            }
+            put(key, value);
         }
-        return ???;
+        return value;
     }
 
     protected abstract V load(K key);
